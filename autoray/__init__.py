fa63337702fef729bd0804f496edde17e3918251
Johnnie Gray <johnniemcgray@gmail.com>
2022-11-20 23:26:23 -0800

    update versioneer

--- autoray/__init__.py
+++ autoray/__init__.py
@@ -57,7 +57,5 @@ __all__ = (
     "lazy",
 )
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
