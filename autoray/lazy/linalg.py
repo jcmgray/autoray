b23212bfe7d0d0eaf3c0113e0bfea8ebe11959c4
Johnnie Gray <johnniemcgray@gmail.com>
2025-07-17 14:27:00 -0700

    LazyArray: support broadcasted linalg

--- autoray/lazy/linalg.py
+++ autoray/lazy/linalg.py
@@ -19,11 +19,11 @@ def svd(a):
     a = ensure_lazy(a)
     fn_svd = get_lib_fn(a.backend, "linalg.svd")
     lsvd = a.to(fn_svd, (a,), shape=(3,))
-    m, n = shape(a)
+    *b, m, n = shape(a)
     k = min(m, n)
-    lU = lsvd.to(operator.getitem, (lsvd, 0), shape=(m, k))
-    ls = lsvd.to(operator.getitem, (lsvd, 1), shape=(k,))
-    lV = lsvd.to(operator.getitem, (lsvd, 2), shape=(k, n))
+    lU = lsvd.to(operator.getitem, (lsvd, 0), shape=(*b, m, k))
+    ls = lsvd.to(operator.getitem, (lsvd, 1), shape=(*b, k))
+    lV = lsvd.to(operator.getitem, (lsvd, 2), shape=(*b, k, n))
     return lU, ls, lV
 
 
@@ -31,10 +31,10 @@ def svd(a):
 def qr(a):
     a = ensure_lazy(a)
     lQR = a.to(get_lib_fn(a.backend, "linalg.qr"), (a,), shape=(2,))
-    m, n = shape(a)
+    *b, m, n = shape(a)
     k = min(m, n)
-    lQ = lQR.to(operator.getitem, (lQR, 0), shape=(m, k))
-    lR = lQR.to(operator.getitem, (lQR, 1), shape=(k, n))
+    lQ = lQR.to(operator.getitem, (lQR, 0), shape=(*b, m, k))
+    lR = lQR.to(operator.getitem, (lQR, 1), shape=(*b, k, n))
     return lQ, lR
 
 
@@ -43,9 +43,16 @@ def eig(a):
     a = ensure_lazy(a)
     fn_eig = get_lib_fn(a.backend, "linalg.eig")
     leig = a.to(fn_eig, (a,), shape=(2,))
-    m = shape(a)[0]
-    el = leig.to(operator.getitem, (leig, 0), shape=(m,))
-    ev = leig.to(operator.getitem, (leig, 1), shape=(m, m))
+    *b, m, _ = shape(a)
+    el = leig.to(
+        operator.getitem,
+        (leig, 0),
+        shape=(
+            *b,
+            m,
+        ),
+    )
+    ev = leig.to(operator.getitem, (leig, 1), shape=(*b, m, m))
     return el, ev
 
 
@@ -54,9 +61,16 @@ def eigh(a):
     a = ensure_lazy(a)
     fn_eigh = get_lib_fn(a.backend, "linalg.eigh")
     leigh = a.to(fn_eigh, (a,), shape=(2,))
-    m = shape(a)[0]
-    el = leigh.to(operator.getitem, (leigh, 0), shape=(m,))
-    ev = leigh.to(operator.getitem, (leigh, 1), shape=(m, m))
+    *b, m, _ = shape(a)
+    el = leigh.to(
+        operator.getitem,
+        (leigh, 0),
+        shape=(
+            *b,
+            m,
+        ),
+    )
+    ev = leigh.to(operator.getitem, (leigh, 1), shape=(*b, m, m))
     return el, ev
 
 
@@ -76,6 +90,7 @@ def cholesky(a):
 
 @lazy_cache("linalg.solve")
 def solve(a, b):
+    # XXX: support non square a?
     a = ensure_lazy(a)
     b = ensure_lazy(b)
     backend = find_common_backend(a, b)
